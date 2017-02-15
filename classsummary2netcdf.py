198d1faeaece5ddea6b94ccc5648a9de09201335
Joe Futrelle <joefutrelle@gmail.com>
2017-02-15 16:08:34 -0500

    script to process a directory of yearly class summaries

---
+++ classsummary2netcdf.py
@@ -0,0 +1,42 @@
+import sys
+import os
+
+import yaml
+from munch import munchify
+
+# clone http://github.com/joefutrelle/netcdf-cf-utils
+# at the same level as this repo, for this to work
+sys.path.append(os.path.join('..','netcdf-cf-utils'))
+
+from ott.netcdf import IfcbMetadata, csdir2netcdf
+
+# constants
+CONFIG_FILE = 'config.yml'
+
+def load_config(config_file):
+    with open(config_file) as fin:
+        return munchify(yaml.safe_load(fin))
+
+def process_dir(in_dir, out_dir):
+    # load configuration
+    config = load_config(CONFIG_FILE)
+
+    # configure metadata
+    md = IfcbMetadata(**config.metadata)
+
+    # configure summary processing
+    freq = config.summary.frequency
+    thresh = config.summary.threshold
+
+    try:
+        os.makedirs(out_dir)
+    except:
+        pass
+
+    csdir2netcdf(in_dir, out_dir, frequency=freq, threshold=thresh, metadata=md)
+
+if __name__=='__main__':
+    assert len(sys.argv) == 3, "must specify an input and output directory"
+    in_dir = sys.argv[1]
+    out_dir = sys.argv[2]
+    process_dir(in_dir, out_dir)
