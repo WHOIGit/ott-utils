16716378b942cf1486b2bb0bc5c481a79bdac5cd
Joe Futrelle <joefutrelle@gmail.com>
2018-09-12 09:43:51 -0400

    adding comments

--- utilities/merge_ml_analyzed.py
+++ utilities/merge_ml_analyzed.py
@@ -7,6 +7,10 @@ from ott.class_scores import merge_ml_analyzed
 from ott.class_summary import ClassSummary 
 from ott.common import config_logging
 
+"""Given a count summary and ml_analyzed summary, merge the two so that
+the count summary contains the ml_analyzed data, this is necessary for
+producing concentrations product."""
+
 if __name__=='__main__':
     ap = ArgumentParser()
     ap.add_argument('count_summary', help='count summary json file')
@@ -25,6 +29,10 @@ if __name__=='__main__':
     logging.info('merging ml_analyzed with counts')
 
     merge_ml_analyzed(count_summary, ml_analyzed)
+
+    # if an output file is specified, write to that file.
+    # otherwise overwrite the existing count summary with the
+    # summary containing the ml_analyzed data
 
     if args.output is None:
         with open(args.count_summary,'w') as fout:
